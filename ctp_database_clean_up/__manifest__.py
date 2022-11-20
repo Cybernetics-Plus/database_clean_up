fcff1df9431494bd3556a0923b552647761da418
BOMEN <bomen21x@gmail.com>
2022-11-20 16:07:34 +0700

    ctp_database_clean_up

--- ctp_database_clean_up/__manifest__.py
+++ ctp_database_clean_up/__manifest__.py
@@ -7,27 +7,27 @@
 ###################################################################################
 
 {
-    'name': 'Database Clean up',
-    'version': '15.0.1.0.1',
-    'summary': """ 
+    "name": "Database Clean up",
+    "version": "15.0.1.0.1",
+    "summary": """ 
             Cybernetics Plus Tools Database Clean up
             .""",
-    'description': """ 
+    "description": """ 
             Cybernetics Plus Tools Database Clean up
             .""",
-    'author': 'Cybernetics Plus Co., Ltd.',
-    'website': 'https://www.cybernetics.plus',
-    'live_test_url': 'https://www.cybernetics.plus',
-    'images': ['static/description/banner.png'],
-    'category': 'Tools',
-    'license': 'LGPL-3',
-    'installable': True,
-    'application': False,
-    'auto_install': False,
-    'contributors': [
-        'Developer <dev@cybernetics.plus>',
+    "author": "Cybernetics+",
+    "website": "https://www.cybernetics.plus",
+    "live_test_url": "https://www.cybernetics.plus",
+    "images": ["static/description/banner.png"],
+    "category": "Tools",
+    "license": "LGPL-3",
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "contributors": [
+        "Developer <dev@cybernetics.plus>",
     ],
-    'data': [
-        'views/ctp_database_clean_up.xml',
+    "data": [
+        "views/ctp_database_clean_up.xml",
     ],
 }
